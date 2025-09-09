0b188fa5d77f33c8abd331453818252c24a1fb7a
Felipe C. de Pauli <fcdpauli@gmail.com>
2025-09-09 10:24:52 -0300

    Init

---
+++ 01_basics/01.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+
+
+int main(int argc, char* argv[]) {
+
+    std::cout << "------------------------------------------------------------" << std::endl;
+    std::cout << "---------------- THE SYSTEM - THE EVIL!!! ------------------" << std::endl;
+    std::cout << "------------------------------------------------------------" << std::endl;
+    std::cout << "E aí, paizão!? Tamo na área! Chega mais que vamos sorver esta porra finalmente! Não vem de 5a série não, seu mala! Toma tento, doidão!! É conhecimento, pae!" << std::endl;
+    std::cout << "------------------------------------------------------------" << std::endl;
+
+
+    return 0;
+}
